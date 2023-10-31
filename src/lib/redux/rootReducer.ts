68101973f4b56dffc75e7782dbd52640dd7758b5
Gopesh Kumar Jangid <gopesh.jangid@gmail.com>
2023-10-31 19:45:33 +0530

    added redux toolkit and persist

--- src/lib/redux/rootReducer.ts
+++ src/lib/redux/rootReducer.ts
@@ -1,6 +1,7 @@
 /* Instruments */
+import { combineReducers } from '@reduxjs/toolkit';
 import { counterSlice } from './slices'
 
-export const reducer = {
+export const reducer = combineReducers({
   counter: counterSlice.reducer,
-}
+});
--- src/lib/redux/store.ts
+++ src/lib/redux/store.ts
@@ -1,31 +1,84 @@
 /* Core */
-import { configureStore, type ThunkAction, type Action } from '@reduxjs/toolkit'
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { useMemo } from 'react';
 import {
   useSelector as useReduxSelector,
   useDispatch as useReduxDispatch,
-  type TypedUseSelectorHook,
-} from 'react-redux'
+  TypedUseSelectorHook,
+} from 'react-redux';
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 /* Instruments */
-import { reducer } from './rootReducer'
-import { middleware } from './middleware'
+import { reducer } from './rootReducer';
+import { middleware } from './middleware';
 
-export const reduxStore = configureStore({
-  reducer,
-  middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(middleware)
-  },
-})
-export const useDispatch = () => useReduxDispatch<ReduxDispatch>()
-export const useSelector: TypedUseSelectorHook<ReduxState> = useReduxSelector
+const persistConfig = {
+  key: 'floyxData',
+  storage,
+  whitelist: ['floyx_data'], // place to select which state you want to persist
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+let store: any;
+
+function makeStore(initialState = {}) {
+  return configureStore({
+    reducer: persistedReducer, // corrected line
+    preloadedState: initialState, // corrected line
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }).concat(middleware),
+  });
+}
+
+export const initializeStore = (preloadedState: any) => {
+  let _store = store ?? makeStore(preloadedState);
+
+   if (preloadedState && store) {
+    _store = makeStore({
+      ...store.getState(),
+      ...preloadedState,
+    })
+    // Reset the current store
+    store = undefined
+  }
+
+  // For SSG and SSR always create a new store
+  if (typeof window === 'undefined') return _store
+  // Create the store once in the client
+  if (!store) store = _store
+
+  return _store
+};
+
+export const useDispatch = () => useReduxDispatch<ReduxDispatch>();
+export const useSelector: TypedUseSelectorHook<ReduxState> = useReduxSelector;
 
 /* Types */
-export type ReduxStore = typeof reduxStore
-export type ReduxState = ReturnType<typeof reduxStore.getState>
-export type ReduxDispatch = typeof reduxStore.dispatch
+export type ReduxStore = ReturnType<typeof makeStore>;
+export type ReduxState = ReturnType<ReduxStore['getState']>;
+export type ReduxDispatch =  typeof store.dispatch;
 export type ReduxThunkAction<ReturnType = void> = ThunkAction<
   ReturnType,
   ReduxState,
   unknown,
   Action
->
+>;
+
+export function useStore(initialState: any) {
+  const store = useMemo(() => initializeStore(initialState), [initialState]);
+  return store;
+}
