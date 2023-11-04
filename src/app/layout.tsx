88cad36c9297b7f7dd44e42a0a6b9d80d7213686
brijesh <brijeshthakkar178@gmail.com>
2023-11-04 19:52:53 +0530

    feat: 🎸 next auth setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 
 import ThemeRegistry from '@/components/ThemeRegistry/ThemeRegistry';
 import '../index.scss';
+import AuthProvider from './context/AuthProvider';
 
 export default function RootLayout({
   children,
@@ -20,7 +21,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${selectedTheme}-theme`}>
-        <ThemeRegistry>{children}</ThemeRegistry>
+        <AuthProvider>
+          <ThemeRegistry>{children}</ThemeRegistry>
+        </AuthProvider>
       </body>
     </html>
   );
