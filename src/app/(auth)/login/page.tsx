5ca31a882ab67a43533dd2fec9292ef0f599afa2
brijesh <brijeshthakkar178@gmail.com>
2023-11-06 23:04:29 +0530

    feat: 🎸 prevent login for authenticated users

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -21,7 +21,7 @@ import {
   styled,
   useTheme,
 } from '@mui/material';
-import { signIn } from 'next-auth/react';
+import { signIn, useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 
 import LoginImage from '../social-login/components/login-image';
@@ -74,6 +74,7 @@ interface IFormError {
 }
 
 const Login: FC = () => {
+  const session = useSession();
   const { palette } = useTheme();
   const router = useRouter();
   const [loading, setLoading] = useState<boolean>(false);
@@ -155,198 +156,204 @@ const Login: FC = () => {
     return Object.values(err).every(value => value === '');
   };
 
+  if (session.status === 'authenticated') {
+    router.replace(allRoutes.home);
+  }
+
   return (
-    <LoginWrapper>
-      <Grid container minHeight="100vh">
-        <LoginImage />
-        <Grid item md={6} sm={12} zIndex="1">
-          <Box
-            textAlign="center"
-            padding={{ md: '47px 15px 40px', xs: '38px 25px 38px' }}
-          >
-            <Typography
-              variant="h5"
-              fontSize="16px"
-              color={palette.text.primary}
-              marginBottom="26px"
-            >
-              Join for free today and keep your data safe in the digital Space{' '}
-            </Typography>
+    session.status === 'unauthenticated' && (
+      <LoginWrapper>
+        <Grid container minHeight="100vh">
+          <LoginImage />
+          <Grid item md={6} sm={12} zIndex="1">
             <Box
-              display="flex"
-              flexDirection="column"
-              gap="24px"
-              maxWidth="360px"
-              marginInline="auto"
+              textAlign="center"
+              padding={{ md: '47px 15px 40px', xs: '38px 25px 38px' }}
             >
-              <Box mb="3px">
-                <Button
-                  variant="outlined"
-                  className="outline-btn"
-                  onClick={() => router.push(allRoutes.register)}
-                >
-                  Create an account
-                </Button>
-              </Box>
               <Typography
-                variant="h3"
-                fontSize="24px"
-                fontWeight="600"
+                variant="h5"
+                fontSize="16px"
                 color={palette.text.primary}
-                textAlign="left"
+                marginBottom="26px"
               >
-                Login to your account
+                Join for free today and keep your data safe in the digital Space{' '}
               </Typography>
-              <Box component="form" m={0} noValidate onSubmit={login}>
-                <FormControl>
-                  <FormLabel>Username or email </FormLabel>
-                  <TextField
-                    name="email"
-                    fullWidth
-                    hiddenLabel
-                    placeholder="Ex. Dustin Max"
-                    onChange={onChangeHandler}
-                    error={!!formError.email}
-                    helperText={formError.email}
-                    InputProps={{
-                      startAdornment: (
-                        <InputAdornment position="end">
-                          <IconButton edge="end" color="primary">
-                            <Image src={iconUser} alt="" />
-                          </IconButton>
-                        </InputAdornment>
-                      ),
-                    }}
-                  />
-                </FormControl>
-                <FormControl>
-                  <Box
-                    display="flex"
-                    justifyContent="space-between"
-                    alignItems="start"
-                    sx={{ '& label': { marginBottom: '0 !important' } }}
-                    mb={1.5}
-                  >
-                    <FormLabel>Password</FormLabel>
-                    <Typography
-                      fontSize="16px"
-                      fontWeight="400"
-                      sx={{ '& a': { color: palette.primary.main } }}
-                    >
-                      <Link href={allRoutes.login}>
-                        Forgotten your password?
-                      </Link>
-                    </Typography>
-                  </Box>
-                  <TextField
-                    fullWidth
-                    hiddenLabel
-                    placeholder="************"
-                    type="password"
-                    name="password"
-                    onChange={onChangeHandler}
-                    error={!!formError.password}
-                    helperText={formError.password}
-                    InputProps={{
-                      startAdornment: (
-                        <InputAdornment position="end">
-                          <IconButton edge="end" color="primary">
-                            <Image src={iconLock} alt="" />
-                          </IconButton>
-                        </InputAdornment>
-                      ),
-                    }}
-                  />
-                </FormControl>
-                <FormControl>
+              <Box
+                display="flex"
+                flexDirection="column"
+                gap="24px"
+                maxWidth="360px"
+                marginInline="auto"
+              >
+                <Box mb="3px">
                   <Button
-                    variant="contained"
-                    color="primary"
-                    type="submit"
-                    sx={{
-                      textTransform: 'capitalize',
-                      padding: '12px 29px',
-                      color: palette.background.default,
-                      fontSize: '16px',
-                      fontWeight: '400',
-                    }}
+                    variant="outlined"
+                    className="outline-btn"
+                    onClick={() => router.push(allRoutes.register)}
                   >
-                    {loading ? (
-                      <>
-                        <CircularProgress size={24} color="inherit" />
-                        Submit
-                      </>
-                    ) : (
-                      'Submit'
-                    )}
+                    Create an account
                   </Button>
-                </FormControl>
-                <FormControl sx={{ marginBottom: '0 !important' }}>
-                  <FormControlLabel
-                    name="remember"
-                    control={
-                      <Checkbox
-                        defaultChecked={false}
-                        onChange={onChangeHandler}
-                      />
-                    }
-                    label="Remember me"
-                  />
-                </FormControl>
+                </Box>
+                <Typography
+                  variant="h3"
+                  fontSize="24px"
+                  fontWeight="600"
+                  color={palette.text.primary}
+                  textAlign="left"
+                >
+                  Login to your account
+                </Typography>
+                <Box component="form" m={0} noValidate onSubmit={login}>
+                  <FormControl>
+                    <FormLabel>Username or email </FormLabel>
+                    <TextField
+                      name="email"
+                      fullWidth
+                      hiddenLabel
+                      placeholder="Ex. Dustin Max"
+                      onChange={onChangeHandler}
+                      error={!!formError.email}
+                      helperText={formError.email}
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="end">
+                            <IconButton edge="end" color="primary">
+                              <Image src={iconUser} alt="" />
+                            </IconButton>
+                          </InputAdornment>
+                        ),
+                      }}
+                    />
+                  </FormControl>
+                  <FormControl>
+                    <Box
+                      display="flex"
+                      justifyContent="space-between"
+                      alignItems="start"
+                      sx={{ '& label': { marginBottom: '0 !important' } }}
+                      mb={1.5}
+                    >
+                      <FormLabel>Password</FormLabel>
+                      <Typography
+                        fontSize="16px"
+                        fontWeight="400"
+                        sx={{ '& a': { color: palette.primary.main } }}
+                      >
+                        <Link href={allRoutes.login}>
+                          Forgotten your password?
+                        </Link>
+                      </Typography>
+                    </Box>
+                    <TextField
+                      fullWidth
+                      hiddenLabel
+                      placeholder="************"
+                      type="password"
+                      name="password"
+                      onChange={onChangeHandler}
+                      error={!!formError.password}
+                      helperText={formError.password}
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="end">
+                            <IconButton edge="end" color="primary">
+                              <Image src={iconLock} alt="" />
+                            </IconButton>
+                          </InputAdornment>
+                        ),
+                      }}
+                    />
+                  </FormControl>
+                  <FormControl>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      type="submit"
+                      sx={{
+                        textTransform: 'capitalize',
+                        padding: '12px 29px',
+                        color: palette.background.default,
+                        fontSize: '16px',
+                        fontWeight: '400',
+                      }}
+                    >
+                      {loading ? (
+                        <>
+                          <CircularProgress size={24} color="inherit" />
+                          Submit
+                        </>
+                      ) : (
+                        'Submit'
+                      )}
+                    </Button>
+                  </FormControl>
+                  <FormControl sx={{ marginBottom: '0 !important' }}>
+                    <FormControlLabel
+                      name="remember"
+                      control={
+                        <Checkbox
+                          defaultChecked={false}
+                          onChange={onChangeHandler}
+                        />
+                      }
+                      label="Remember me"
+                    />
+                  </FormControl>
+                </Box>
+                <Box mt="3px">
+                  <Typography
+                    variant="h6"
+                    fontSize="16px"
+                    fontWeight="400"
+                    lineHeight="24px"
+                    color="#CECED2"
+                    sx={{ '& a': { color: palette.primary.main } }}
+                  >
+                    By signing up,you agree to
+                    <Link href="/"> Terms of Service </Link> and
+                    <Link href="/"> Privacy Policy, </Link>
+                    including <Link href="/"> Cookie Use.</Link>
+                  </Typography>
+                </Box>
+                <Box mt="20px" textAlign="left">
+                  <Link href={allRoutes.socialLogin} className="social-login">
+                    <SVGArrowLeft />
+                    <span className="gradient-text">Back to social login</span>
+                  </Link>
+                </Box>
               </Box>
-              <Box mt="3px">
+              <Box mt="54px">
+                <Stack
+                  spacing={{
+                    md: '42px',
+                    xs: '20px',
+                  }}
+                  mb="13px"
+                  className="login-service"
+                  direction="row"
+                  justifyContent="center"
+                  flexWrap="wrap"
+                >
+                  <Link href="/"> Terms of service</Link>
+                  <Link href="/"> Privacy Policy</Link>
+                  <Link href="/"> Cookie use</Link>
+                </Stack>
                 <Typography
                   variant="h6"
                   fontSize="16px"
                   fontWeight="400"
                   lineHeight="24px"
-                  color="#CECED2"
+                  color="#85838F"
                   sx={{ '& a': { color: palette.primary.main } }}
                 >
-                  By signing up,you agree to
-                  <Link href="/"> Terms of Service </Link> and
-                  <Link href="/"> Privacy Policy, </Link>
-                  including <Link href="/"> Cookie Use.</Link>
+                  © 2022 Powered by Floyx, LLC & <Link href="/"> Polygon.</Link>
                 </Typography>
               </Box>
-              <Box mt="20px" textAlign="left">
-                <Link href={allRoutes.socialLogin} className="social-login">
-                  <SVGArrowLeft />
-                  <span className="gradient-text">Back to social login</span>
-                </Link>
-              </Box>
             </Box>
-            <Box mt="54px">
-              <Stack
-                spacing={{
-                  md: '42px',
-                  xs: '20px',
-                }}
-                mb="13px"
-                className="login-service"
-                direction="row"
-                justifyContent="center"
-                flexWrap="wrap"
-              >
-                <Link href="/"> Terms of service</Link>
-                <Link href="/"> Privacy Policy</Link>
-                <Link href="/"> Cookie use</Link>
-              </Stack>
-              <Typography
-                variant="h6"
-                fontSize="16px"
-                fontWeight="400"
-                lineHeight="24px"
-                color="#85838F"
-                sx={{ '& a': { color: palette.primary.main } }}
-              >
-                © 2022 Powered by Floyx, LLC & <Link href="/"> Polygon.</Link>
-              </Typography>
-            </Box>
-          </Box>
+          </Grid>
         </Grid>
-      </Grid>
-    </LoginWrapper>
+      </LoginWrapper>
+    )
   );
 };
 
--- src/app/(auth)/social-login/page.tsx
+++ src/app/(auth)/social-login/page.tsx
@@ -13,6 +13,7 @@ import {
   useTheme,
 } from '@mui/material';
 import { useRouter } from 'next/navigation';
+import { useSession } from 'next-auth/react';
 
 import LoginImage from './components/login-image';
 import SignInGoogle from './components/sign-in-google';
@@ -42,105 +43,114 @@ const SocialLoginWrapper = styled(Box)(({ theme }: { theme: Theme }) => ({
 const SocialLogin: FC = () => {
   const { palette } = useTheme();
   const router = useRouter();
+  const session = useSession();
+
+  if (session.status === 'authenticated') {
+    router.replace(allRoutes.home);
+  }
+
   return (
-    <SocialLoginWrapper>
-      <Grid container minHeight="100vh">
-        <LoginImage />
-        <Grid item md={6} sm={12}>
-          <Box
-            textAlign="center"
-            padding={{ md: '113px 15px 40px', xs: '38px 25px 38px' }}
-          >
-            <Typography
-              variant="h5"
-              fontSize="16px"
-              color={palette.text.primary}
-              marginBottom="26px"
-            >
-              Join for free today and keep your data safe in the digital Space{' '}
-            </Typography>
+    session.status === 'unauthenticated' && (
+      <SocialLoginWrapper>
+        <Grid container minHeight="100vh">
+          <LoginImage />
+          <Grid item md={6} sm={12}>
             <Box
-              display="flex"
-              flexDirection="column"
-              gap="24px"
-              maxWidth="360px"
-              marginInline="auto"
+              textAlign="center"
+              padding={{ md: '113px 15px 40px', xs: '38px 25px 38px' }}
             >
-              <Box mb="3px">
-                <Button
-                  variant="outlined"
-                  className="outline-btn"
-                  onClick={() => router.push(allRoutes.register)}
-                >
-                  Create an account
-                </Button>
-              </Box>
               <Typography
-                variant="h3"
-                fontSize="24px"
-                fontWeight="600"
+                variant="h5"
+                fontSize="16px"
                 color={palette.text.primary}
-                textAlign="left"
+                marginBottom="26px"
               >
-                Login to your account
+                Join for free today and keep your data safe in the digital Space{' '}
               </Typography>
-              <SignInGoogle />
-              <SignInFacebook />
-              <Box mb="3px">
-                <Button
-                  variant="outlined"
-                  className="outline-btn"
-                  onClick={() => router.push(allRoutes.login)}
+              <Box
+                display="flex"
+                flexDirection="column"
+                gap="24px"
+                maxWidth="360px"
+                marginInline="auto"
+              >
+                <Box mb="3px">
+                  <Button
+                    variant="outlined"
+                    className="outline-btn"
+                    onClick={() => router.push(allRoutes.register)}
+                  >
+                    Create an account
+                  </Button>
+                </Box>
+                <Typography
+                  variant="h3"
+                  fontSize="24px"
+                  fontWeight="600"
+                  color={palette.text.primary}
+                  textAlign="left"
                 >
-                  Use username or email
-                </Button>
+                  Login to your account
+                </Typography>
+                <SignInGoogle />
+                <SignInFacebook />
+                <Box mb="3px">
+                  <Button
+                    variant="outlined"
+                    className="outline-btn"
+                    onClick={() => router.push(allRoutes.login)}
+                  >
+                    Use username or email
+                  </Button>
+                </Box>
+                <Box mt="3px">
+                  <Typography
+                    variant="h6"
+                    fontSize="16px"
+                    fontWeight="400"
+                    lineHeight="24px"
+                    color="#CECED2"
+                    sx={{ '& a': { color: palette.primary.main } }}
+                  >
+                    By signing up,you agree to
+                    <Link href="/"> Terms of Service </Link> and
+                    <Link href="/"> Privacy Policy, </Link>
+                    including <Link href="/"> Cookie Use.</Link>
+                  </Typography>
+                </Box>
               </Box>
-              <Box mt="3px">
+              <Box mt="54px">
+                <Stack
+                  spacing={{
+                    md: '42px',
+                    xs: '20px',
+                  }}
+                  mb="13px"
+                  className="login-service"
+                  direction="row"
+                  justifyContent="center"
+                  flexWrap="wrap"
+                >
+                  <Link href="/"> Terms of service</Link>
+                  <Link href="/"> Privacy Policy</Link>
+                  <Link href="/"> Cookie use</Link>
+                </Stack>
                 <Typography
                   variant="h6"
                   fontSize="16px"
                   fontWeight="400"
                   lineHeight="24px"
-                  color="#CECED2"
-                  sx={{ '& a': { color: palette.primary.main} }}
+                  color="#85838F"
+                  sx={{ '& a': { color: palette.primary.main } }}
                 >
-                  By signing up,you agree to
-                  <Link href="/"> Terms of Service </Link> and
-                  <Link href="/"> Privacy Policy, </Link>
-                  including <Link href="/"> Cookie Use.</Link>
+                  © 2022 Powered by Floyx, LLC & <Link href="/"> Polygon.</Link>
                 </Typography>
               </Box>
             </Box>
-            <Box mt="54px">
-              <Stack
-                spacing={{
-                  md: '42px',
-                  xs: '20px',
-                }}
-                mb="13px"
-                className="login-service"
-                direction="row"
-                justifyContent="center"
-                flexWrap='wrap' >
-                <Link href="/"> Terms of service</Link>
-                <Link href="/"> Privacy Policy</Link>
-                <Link href="/"> Cookie use</Link>
-              </Stack>
-              <Typography
-                variant="h6"
-                fontSize="16px"
-                fontWeight="400"
-                lineHeight="24px"
-                color="#85838F"
-                sx={{ '& a': { color: palette.primary.main} }}
-              >
-                © 2022 Powered by Floyx, LLC & <Link href="/"> Polygon.</Link>
-              </Typography>
-            </Box>
-          </Box>
+          </Grid>
         </Grid>
-      </Grid>
-    </SocialLoginWrapper>
+      </SocialLoginWrapper>
+    )
   );
 };
 
