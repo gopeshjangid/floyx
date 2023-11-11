483a37d1c241d200a5c4c3c68776700806f147c0
brijesh <brijeshthakkar178@gmail.com>
2023-11-11 16:31:04 +0530

    fix: 🐛 main btanch theme conflicts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,19 @@
-'use client';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-import ThemeRegistry from '@/components/ThemeRegistry/ThemeRegistry';
 import '../index.scss';
-import AuthProvider from './context/AuthProvider';
 import { ToastProvider } from '@/components/Toast/useToast';
+import AuthProvider from './context/AuthProvider';
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [selectedTheme, setTheme] = useState('light');
-
-  useEffect(() => {
-    if (localStorage.getItem('theme')) {
-      setTheme(localStorage.getItem('theme')!);
-    }
-  }, []);
-
   return (
     <html lang="en">
-      <body className={`${selectedTheme}-theme`}>
+      <body>
         <ToastProvider>
-          <AuthProvider>
-            <ThemeRegistry>{children}</ThemeRegistry>
-          </AuthProvider>
+          <AuthProvider>{children}</AuthProvider>
         </ToastProvider>
       </body>
     </html>
---
+++ src/components/ThemeRegistry/PageProvider.tsx
@@ -0,0 +1,26 @@
+"use client"
+import { ThemeProvider as PreferredThemeProvider } from "next-themes";
+import Head from "next/head";
+import { FC } from "react";
+import NextAppDirEmotionCacheProvider from "./EmotionCache";
+import MUIThemeProvider from "./MuiThemeProvider";
+
+
+interface PageProviderProps {
+  children: React.ReactNode;
+}
+
+const PageProvider: FC<PageProviderProps> = ({
+  children, 
+}) => (
+  <PreferredThemeProvider enableSystem>
+    <NextAppDirEmotionCacheProvider options={{ key: "mui",prepend: true }}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <MUIThemeProvider>{children}</MUIThemeProvider>
+    </NextAppDirEmotionCacheProvider>
+  </PreferredThemeProvider>
+);
+
+export default PageProvider;
