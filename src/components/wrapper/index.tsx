53c1f04e428d3150f436421c612f85b0542b2068
brijesh <brijeshthakkar178@gmail.com>
2023-11-16 15:54:11 +0530

    fix: 🐛 inline style

--- src/components/wrapper/index.tsx
+++ src/components/wrapper/index.tsx
@@ -1,22 +1,14 @@
 import React, { ReactNode } from 'react';
-import { Box, useTheme } from '@mui/material';
+import { Box, styled, Theme } from '@mui/material';
 
+const WrapperContainer = styled(Box)(({ theme }: { theme: Theme }) => ({
+  width: '100%',
+  borderRadius: '10px',
+  bgcolor: theme.palette?.mode === 'light' ? '#fff' : '#0B081F',
+  border: `1px solid ${theme.palette?.mode === 'light' ? '#E7F0FC' : 'rgba(255, 255, 255, 0.15)'}`,
+}));
 const Wrapper = ({ children, ...rest }: { children: ReactNode; mb?: any }) => {
-  const { palette } = useTheme();
-
-  return (
-    <Box
-      sx={{
-        width: '100%',
-        borderRadius: '10px',
-        bgcolor: palette?.mode === 'light' ? '#fff' : '#0B081F',
-        border: `1px solid ${palette?.mode === 'light' ? '#E7F0FC' : 'rgba(255, 255, 255, 0.15)'}`,
-      }}
-      {...rest}
-    >
-      {children}
-    </Box>
-  );
+  return <WrapperContainer {...rest}>{children}</WrapperContainer>;
 };
 
 export default Wrapper;
