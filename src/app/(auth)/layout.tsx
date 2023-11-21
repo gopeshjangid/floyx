b362f17b69ca3d48f2394c0bf7cb83ffdbf9b8ec
brijesh <brijeshthakkar178@gmail.com>
2023-11-21 19:55:00 +0530

    refactor: 💡 common auth layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,1 +1,24 @@
-// TODO: add auth layout
+import React from 'react';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+import { Grid } from '@mui/material';
+
+import { allRoutes } from '@/constants/allRoutes';
+import LoginImage from './social-login/components/login-image';
+import { AuthWrapper } from './styled';
+
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const session = await getServerSession();
+
+  if (session) {
+    redirect(allRoutes.home);
+  }
+  return (
+    <AuthWrapper>
+      <Grid container minHeight="100vh">
+        <LoginImage />
+        {children}
+      </Grid>
+    </AuthWrapper>
+  );
+}
