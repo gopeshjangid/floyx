de8a8e2ed91c0c0796c41138dfe78b646935eb27
brijesh <brijeshthakkar178@gmail.com>
2023-11-25 15:31:11 +0530

    fix: 🐛 add middleware next auth secret

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,12 @@
-// import { withAuth } from 'next-auth/middleware';
+import { withAuth } from 'next-auth/middleware';
 
-// export default withAuth({
-//   pages: {
-//     signIn: '/social-login',
-//     error: '/social-login',
-//   },
-// });
-
-export { default } from 'next-auth/middleware';
-
+export default withAuth({
+  pages: {
+    signIn: '/social-login',
+    error: '/social-login',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
 // WARN: Add all protected routes, do not use REGEX and do not try to create function or something that returns array
 export const config = {
   matcher: ['/', '/notifications', '/people', '/settings', '/inbox', '/earnings', '/profile'],
