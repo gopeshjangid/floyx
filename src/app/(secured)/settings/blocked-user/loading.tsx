fa4154cb5923e3ddd92b56f8dfe3a6dc2e46b7e4
brijesh <brijeshthakkar178@gmail.com>
2023-12-02 11:56:12 +0530

    style: 💄 blocked user list loading

--- src/app/(secured)/settings/blocked-user/loading.tsx
+++ src/app/(secured)/settings/blocked-user/loading.tsx
@@ -1,9 +1,71 @@
-import React from 'react'
+'use client';
 
-const BlockedUsersLoading = () => {
+import React from 'react';
+import { Box, List, Skeleton, Theme, styled } from '@mui/material';
+
+import Wrapper from '@/components/wrapper';
+import { SettingWrapper } from '../styled';
+
+const BlockedUserWrapper = styled(Box)(
+  ({ theme }: { theme: Theme }) => ({
+    padding: '0',
+    '& .MuiList-root': {
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      gap: 15,
+      '& .MuiSkeleton-root': {
+        transform: 'scale(1)',
+        height: 50,
+        '&.MuiSkeleton-circular': {
+          width: '50px',
+          height: '50px',
+        },
+        '&.MuiSkeleton-text': { flex: 1 },
+      },
+      [theme.breakpoints.up('md')]: {
+        '& .MuiList-root': {
+          gap: 15,
+        },
+        '& .MuiSkeleton-root': {
+          height: 65,
+          '&.MuiSkeleton-circular': {
+            width: '59px',
+            height: '59px',
+          },
+        },
+      },
+    },
+  })
+);
+
+const BlockedUserLoader = () => {
   return (
-    <div>BlockedUsersLoading</div>
-  )
-}
+    <>
+      <Wrapper
+        sx={{
+          maxWidth: {
+            xs: '100%',
+            sm: '70%',
+          },
+          marginTop: '20px',
+        }}
+      >
+        <SettingWrapper>
+          <BlockedUserWrapper>
+            <List component="ul">
+              {Array.from(new Array(5)).map((_, index) => (
+                <Box key={index} display="flex" gap={2} flexDirection="row">
+                  <Skeleton variant="circular" />
+                  <Skeleton variant="text" />
+                </Box>
+              ))}
+            </List>
+          </BlockedUserWrapper>
+        </SettingWrapper>
+      </Wrapper>
+    </>
+  );
+};
 
-export default BlockedUsersLoading
+export default BlockedUserLoader;
