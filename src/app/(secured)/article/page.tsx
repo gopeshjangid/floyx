87cc1e80ee5450cd3fa34143f9326cfbbbf36d67
Gopesh Kumar Jangid <gxj15@acuitysso.com>
2023-12-06 12:46:40 +0530

    article added

--- src/app/(secured)/article/page.tsx
+++ src/app/(secured)/article/page.tsx
@@ -1,4 +1,5 @@
-import { Suspense } from 'react';
+'use client'
+import React, { Suspense, useState } from 'react';
 import { Grid } from '@mui/material';
 import LoadingArticleHead from './loading';
 import ArticleHead from '@/components/articleHead';
@@ -6,12 +7,13 @@ import ArticleContent from '@/components/articleContent';
 import PostHeader from "@/components/PostHeader";
 
 export default function Page() {
+  const [articleList, setArticleList] = useState([])
   return (
     <Grid sx={{ width:  { xs: '100%', sm: '100%' }, padding: '0 20px' }}>
       <PostHeader />
-      <ArticleHead />
+      <ArticleHead setArticleList={setArticleList}/>
       <Suspense fallback={<LoadingArticleHead />}>
-        <ArticleContent />
+        <ArticleContent articleList={articleList}/>
       </Suspense>
     </Grid>
   );
--- src/app/(secured)/composer/create/page.tsx
+++ src/app/(secured)/composer/create/page.tsx
@@ -9,7 +9,7 @@ import AddArticleForm from '@/components/addArticleForm';
 export default function Page() {
   const [saveDraft, setSaveDraft] = useState(false);
   return (
-    <Box>
+    <Box sx={{width:'100%', marginTop:'20px'}}>
       <AddArticleHead setSaveDraft={setSaveDraft} />
       <AddArticleForm saveDraft={saveDraft} setSaveDraft={setSaveDraft} />
     </Box>
