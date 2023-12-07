69a0dd2d9a3af1efc58002a22380cef459bd21bf
brijesh <brijeshthakkar178@gmail.com>
2023-12-07 22:03:40 +0530

    feat: 🎸 wrapper layout

--- src/components/wrapper/index.tsx
+++ src/components/wrapper/index.tsx
@@ -5,9 +5,20 @@ const WrapperContainer = styled(Box)(({ theme }: { theme: Theme }) => ({
   width: '100%',
   borderRadius: '10px',
   background: theme.palette?.mode === 'light' ? '#fff' : '#0B081F',
-  border: `1px solid ${theme.palette?.mode === 'light' ? '#E7F0FC' : 'rgba(255, 255, 255, 0.15)'}`,
+  border: `1px solid ${
+    theme.palette?.mode === 'light' ? '#E7F0FC' : 'rgba(255, 255, 255, 0.15)'
+  }`,
+  marginTop: '20px',
+  maxWidth: '98%',
 }));
-const Wrapper = ({ children, ...rest }: { children: ReactNode; mb?: any; sx?: SxProps<Theme> | undefined }) => {
+const Wrapper = ({
+  children,
+  ...rest
+}: {
+  children: ReactNode;
+  mb?: any;
+  sx?: SxProps<Theme> | undefined;
+}) => {
   return <WrapperContainer {...rest}>{children}</WrapperContainer>;
 };
 
