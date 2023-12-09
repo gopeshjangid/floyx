b85ebe8da431c17f84eea7e0ce99745eb6cd807a
Gopesh Kumar Jangid <gopesh.jangid@gmail.com>
2023-12-09 20:51:48 +0530

    lint fixed

--- src/app/(secured)/profile/_components/experience.tsx
+++ src/app/(secured)/profile/_components/experience.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-vars @ts-ignore */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 'use client';
 import React, { useCallback, useEffect, useState } from 'react';
 import {
