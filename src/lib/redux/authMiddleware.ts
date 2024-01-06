4b835758c5a3805421f323d1efe4b9955b0bf65f
Gopesh Kumar Jangid <gopesh.jangid@gmail.com>
2024-01-06 19:49:53 +0530

    fixed rtk middleware

--- src/lib/redux/authMiddleware.ts
+++ src/lib/redux/authMiddleware.ts
@@ -1,9 +1,17 @@
-import { Middleware } from '@reduxjs/toolkit';
+import { Middleware, isRejectedWithValue } from '@reduxjs/toolkit';
 import { signOut } from 'next-auth/react';
 
 export const authMiddleware: Middleware = store => next => action => {
-  if (action.type.endsWith('rejected') && action.error?.status === 401) {
-    signOut({ redirect: false });
+  if (isRejectedWithValue(action)) {
+    // Accessing the status code from the error object
+    const statusCode = action.error?.message?.startsWith('Rejected')
+      ? parseInt(action.error.message.split(' ')[1])
+      : null;
+    console.log('statusCode', statusCode);
+    if (statusCode === 401) {
+      // Handle the 401 unauthorized error
+      signOut({ redirect: false });
+    }
   }
 
   return next(action);
