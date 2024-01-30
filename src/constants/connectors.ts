a688a0d824c6b561e7539801f1f4ab2e849e5bb0
Gopesh Kumar Jangid <gopesh.jangid@gmail.com>
2024-01-30 22:51:18 +0530

    fixed lint errors

--- src/constants/connectors.ts
+++ src/constants/connectors.ts
@@ -1,35 +1,27 @@
-import { InjectedConnector } from '@web3-react/injected-connector'
-import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
-import { WalletLinkConnector } from '@web3-react/walletlink-connector'
+import { InjectedConnector } from '@web3-react/injected-connector';
+import { WalletConnectConnector } from '@web3-react/walletconnect-connector';
+import { WalletLinkConnector } from '@web3-react/walletlink-connector';
 
 const RPC_URLS = {
-  // 1: 'https://mainnet.infura.io/v3/55d040fb60064deaa7acc8e320d99bd4',
-  4: 'https://rinkeby.infura.io/v3/55d040fb60064deaa7acc8e320d99bd4'
-}
+  4: 'https://rinkeby.infura.io/v3/55d040fb60064deaa7acc8e320d99bd4',
+};
 
 //metamask
 export const injected = new InjectedConnector({
-  supportedChainIds: [
-    //  1,
-    //  3,
-    4
-    //   5,
-    //   42
-  ]
-})
+  supportedChainIds: [4],
+});
 
 export const walletconnect = new WalletConnectConnector({
   rpc: {
-    // 1: RPC_URLS[1],
-    4: RPC_URLS[4]
+    4: RPC_URLS[4],
   },
   qrcode: true,
-  pollingInterval: 15000
-})
+  pollingInterval: 15000 as any,
+});
 
 export function resetWalletConnector(connector) {
   if (connector && connector instanceof WalletConnectConnector) {
-    connector.walletConnectProvider = undefined
+    connector.walletConnectProvider = undefined;
   }
 }
 
@@ -37,5 +29,5 @@ export function resetWalletConnector(connector) {
 export const walletlink = new WalletLinkConnector({
   url: RPC_URLS[4],
   appName: 'demo-app',
-  supportedChainIds: [4]
-})
+  supportedChainIds: [4],
+});
