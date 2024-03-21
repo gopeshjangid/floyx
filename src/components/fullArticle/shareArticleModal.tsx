0ec39e8f0ff4b8902ea792fceaf50e8876c4f943
Amarswain450 <amarswain450@gmail.com>
2024-03-21 12:01:36 +0530

    changed border color

--- src/components/fullArticle/shareArticleModal.tsx
+++ src/components/fullArticle/shareArticleModal.tsx
@@ -30,7 +30,7 @@ const style = {
   maxWidth: '50vw',
   overflowY: 'scroll',
   bgcolor: 'background.paper',
-  border: '2px solid #000',
+  border: '2px solid gray',
   boxShadow: 24,
   padding: 3,
   borderRadius: '10px',
