a9121de35d3e1bc26128513c86411f934b2b4b4d
Mr Khokar <Omar.nepa@gmail>
2024-03-24 20:55:52 +0530

    text changes

--- src/components/home/components/Animations/CommentsCard/cards.tsx
+++ src/components/home/components/Animations/CommentsCard/cards.tsx
@@ -119,32 +119,7 @@ export const CommentsCards = () => {
     centerPadding: "60px",
     slidesToShow: 3,
     speed: 500,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+    
   };
   return (
     <> <MainContainer>
--- src/components/home/components/Animations/TypingAnimations/index.tsx
+++ src/components/home/components/Animations/TypingAnimations/index.tsx
@@ -1,12 +1,35 @@
-import React, { useEffect,useState,useRef } from "react";
+import React, { useMemo,useEffect,useState,useRef } from "react";
 import { TypeAnimation } from 'react-type-animation';
 export const TypingAnimation = () => {
+  const [fontSize, setFontSize] = useState(50); // Default font size
   
+   useEffect(() => {
+    // Function to calculate and update font size based on viewport width
+    function updateFontSize() {
+       const viewportWidth = window.innerWidth;
+      let newSize = 50; // Default font size
+             if (viewportWidth < 400) {
+         newSize = 30; // For smaller screens
+      }
+      setFontSize(newSize);
+    }
+     // Call the function initially and add event listener for resize
+    updateFontSize();
+    window.addEventListener('resize', updateFontSize);
+//debugger
+    // Cleanup function to remove event listener
+         return () => {
+       window.removeEventListener('resize', updateFontSize);
+    };
+  }, [window.innerWidth]); // Empty dependency array ensures the effect runs only once
+
+
     return (
          <div className="type-animation-parent"> 
          <div className="child-type">
 
-         <TypeAnimation
+       <TypeAnimation
+         className="abs"
         sequence={[
           'Time for a Revolution in The\n Social Media Industry!',
         1000,
@@ -14,13 +37,12 @@ export const TypingAnimation = () => {
          
         ]}
         speed={20}
-            style={{ whiteSpace: 'pre-line', fontSize: '50px', fontWeight: "700"}}
+             style={{ whiteSpace: 'pre-line', fontSize: `${fontSize}px`, fontWeight: "700"}}
         repeat={Infinity}
       />
          </div>
       
       </div>
-    );
+    )
 };
 
-// "",          1000,          "",
